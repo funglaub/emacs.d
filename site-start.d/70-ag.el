d0b26bc058ddbadfb3a3144f067139e25696c6bb
Florian Unglaub <florian.unglaub@nix-wie-weg.de>
2014-01-10 13:08:48 +0100

    ag shortcuts

--- site-start.d/70-ag.el
+++ site-start.d/70-ag.el
@@ -1,3 +1,10 @@
 (require 'ag)
+
+(defun funglaub-todos-in-project ()
+  (interactive)
+  ;; (ag/search "TODO((?!Prio).)*:" (ag/project-root default-directory ) t))
+  (ag-project-regexp "TODO((?!Prio).)*:"))
+
 (global-set-key (kbd "<f5>") 'ag-project-at-point)
 (global-set-key (kbd "<f6>") 'ag-regexp-project-at-point)
+(global-set-key (kbd "<f7>") 'funglaub-todos-in-project)
