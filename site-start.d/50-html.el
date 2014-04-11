adf8bef971cbcf7895499e30826480e58f26d219
Florian Unglaub <f.unglaub@gmail.com>
2014-04-11 21:38:05 +0200

    handlebars

--- site-start.d/50-html.el
+++ site-start.d/50-html.el
@@ -1,2 +1,7 @@
 (require 'handlebars-sgml-mode)
 (handlebars-use-mode 'global)
+
+(add-to-list 'auto-mode-alist '("\\.hbs$" . html-mode))
+(add-to-list 'auto-mode-alist '("\\.handlebars$" . html-mode))
+
+(add-hook 'html-mode-hook (lambda()(handlebars-sgml-minor-mode 1)))
