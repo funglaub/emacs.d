3777dd9498de9c3bd58a62f78d2870011c03a25d
Florian Unglaub <f.unglaub@gmail.com>
2015-03-04 00:25:42 +0100

    remove legacy code

--- init.el
+++ init.el
@@ -12,14 +12,6 @@
 (autoload 'my-site-start "~/.emacs.d/site-lisp/my-site-start.el" nil t)
 (my-site-start "~/.emacs.d/site-start.d/")
 
-;; Write backup files to own directory
-(setq backup-directory-alist
-      `(("." . ,(expand-file-name
-                 (concat user-emacs-directory "backups")))))
-
-;; Make backups of files, even when they're in version control
-(setq vc-make-backup-files t)
-
 (setq auto-save-default nil)
 
 ;; TODO: Is this still working?
