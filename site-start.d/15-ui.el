f2ccd85b096b95ade606b3bb4df3f1f49c79407e
Florian Unglaub <florian.unglaub@nix-wie-weg.de>
2015-04-10 11:43:20 +0200

    Theme configuration

--- site-start.d/15-ui.el
+++ site-start.d/15-ui.el
@@ -26,7 +26,9 @@
 (setq moe-theme-resize-markdown-title '(1.5 1.4 1.3 1.2 1.0 1.0))
 
 ;; Choose a color for mode-line.(Default: blue)
-(moe-theme-set-color 'blue)
+(moe-theme-set-color 'orange)
+(setq moe-theme-highlight-buffer-id t)
+
 
 ;; Finally, apply moe-theme now.
 ;; Choose what you like, (moe-light) or (moe-dark)
